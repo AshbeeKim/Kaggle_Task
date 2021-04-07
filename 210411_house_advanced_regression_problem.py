5e84827ea4d8a396c335a1c0f6ccd6fe88415732
AshbeeKim <co_m@gnu.ac.kr>
2021-04-07 18:25:19 +0900

    1

---
+++ 210411_house_advanced_regression_problem.py
@@ -0,0 +1,5 @@
+# 2021-04-10 to 2021-04-11
+# HW) Kaggle 숙제 House advanced regression problem
+# (https://www.kaggle.com/c/house-prices-advanced-regression-techniques)
+
+#Flow: load-theory-EDA-Model
